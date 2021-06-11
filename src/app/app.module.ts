6071af8090d0b32e0aea1b2af4f6a1b275b674d9
Hiro-1978 <hiromiuki11@gmail.com>
2021-06-11 14:22:07 +0700

    Angular Homework3 CTF
    Final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { CoverComponent } from './cover/cover.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    NavComponent,
+    CoverComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
